4bd10976952d5a54eda7f12f5543c1402881574f
Yash Ranadive <yash.ranadive@gmail.com>
2015-06-21 23:18:12 -0700

    adding new shell script to parse date from notepad

--- cheat/update_index.sh
+++ cheat/update_index.sh
@@ -1,3 +1,7 @@
+cd ~/parser/cheat
+LINE=`grep -n '~~~' ~/Dropbox/digital_notepad_clean | cut -d ":" -f1`
+NEW_LINE=$((LINE - 1))
+cat ~/Dropbox/digital_notepad_clean | head -$NEW_LINE > files/subset.raw
 ruby run.rb --filein ./files/subset.raw --fileout ./files/subset.json
 curl 'http://localhost:8983/solr/csv/update?stream.body=<delete><query>*:*</query></delete>'
 curl 'http://localhost:8983/solr/csv/update?stream.body=<commit/>'
